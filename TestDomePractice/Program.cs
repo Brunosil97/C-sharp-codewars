72fccb3b4fb44d6c258060bc3820b4206dc7f252
Bruno Silva <brunosilvinosilva@gmail.com>
2020-11-03 15:15:31 +0000

    unique names class done

---
+++ TestDomePractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace TestDomePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+            string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+            Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2)));
+        }
+    }
+
+    public class MergeNames
+    {
+        public static string[] UniqueNames(string[] names1, string[] names2)
+        {
+            var result = names1.Concat(names2);
+            return result.Distinct().ToArray();
+        }
+    }
+}
